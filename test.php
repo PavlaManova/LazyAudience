a75d8851c058e5b98631d2eacd16724da6acd9bc
Teodor Hristov <tedotedo771@gmail.com>
2023-06-26 04:12:32 +0300

    Admin can't do guest things.

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+
+require_once 'connect.php';
+
+$curr_user = new User();
+$guest_events = $curr_user->fetchAllGuestEvents($_SESSION['user_id']);
+$eventId = $_GET['id'];
+$isGuest = false;
+
+foreach ($guest_events as &$e) {
+  if ($e['id'] == $eventId){
+    $isGuest = true;
+    break;
+  }
+}
+
+if (!isset($_SESSION['username']) || !$isGuest) {
+  header('Location: ./loginView.php');
+}
+?>
 <html>
   <head>
     <title>PHP SSE Demo</title>
